7fc13c3dda4347dd869802505a4b297e99671c34
mukeshmaurya <mukesh.maurya@instasolv.com>
2020-05-08 09:26:22 +0530

    Added basic components : Exploring part:2

--- app/src/main/java/com/maurya91/composeway/ui/MainActivity.kt
+++ app/src/main/java/com/maurya91/composeway/ui/MainActivity.kt
@@ -3,35 +3,110 @@ package com.maurya91.composeway.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.ui.core.Alignment
+import androidx.ui.core.ContextAmbient
+import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
-import androidx.ui.foundation.Text
-import androidx.ui.material.MaterialTheme
+import androidx.ui.foundation.*
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.graphics.Color
+import androidx.ui.graphics.imageFromResource
+import androidx.ui.layout.*
+import androidx.ui.material.*
+import androidx.ui.res.imageResource
+import androidx.ui.text.TextStyle
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
+import androidx.ui.unit.sp
+import com.maurya91.composeway.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Greeting("Android")
+                BasicComponents()
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MaterialTheme {
-        Greeting("Android")
+        BasicComponents()
     }
 }
 
+@Composable
+fun BasicComponents() {
+    val resources = ContextAmbient.current.resources
+    VerticalScroller(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize() + Modifier.padding(16.dp)) {
+            Title(title = "Simple Text")
+            Text(text = "Hello Jetpack Compose")
+
+            Title(title = "Styled Text")
+            Text(text = "Hello Jetpack Compose", style = TextStyle(background = Color.Green))
+
+            Title(title = "Button")
+            Button(onClick = {}) {
+                Text(text = "Simple Button")
+            }
+
+            Title(title = "Edit Text")
+            TextField(value = TextFieldValue("jlksdkl"), onValueChange = {})
+
+
+
+            Title(title = "Image")
+//            val image = imageResource(R.drawable.bg_home_gradient)
+            Box(
+                shape = RoundedCornerShape(5.dp),
+                modifier = Modifier.size(72.dp, 56.dp) + Modifier.drawBackground(
+                    color = Color.Magenta
+                )
+            ) {
+                //                Image(asset = imageResource(id = R.drawable.bg_home_gradient))
+            }
+//            Title(title = "Icon")
+//            val icon = imageResource(R.drawable.ic_baseline_arrow_back_24)
+//            Icon(asset = icon,
+//                modifier = Modifier.preferredSize(36.dp, 36.dp),
+//                tint = Color.Blue)
+
+            Title(title = "Card")
+            Card(shape = RoundedCornerShape(4.dp), modifier = Modifier.padding(8.dp)) {
+                ListItem(text = {
+                    Text(text = "Title")
+                }, secondaryText = {
+                    Text(text = "Subtitle")
+                }, icon = {
+                    Box(modifier = Modifier.preferredWidth(48.dp) + Modifier.preferredHeight(48.dp)+Modifier.drawBackground(
+                        color = Color.Green
+                    )) {
+                        //Image(asset = imageFromResource(resources, R.drawable.bg_home_gradient))
+                    }
+                })
+            }
+
+            Title(title = "Circular Progress")
+            CircularProgressIndicator(progress = 0.5f, modifier = Modifier.wrapContentWidth(
+                Alignment.CenterHorizontally))
+        }
+    }
+}
+
+@Composable
+fun Title(title: String) {
+    Text(
+        text = title,
+        color = Color.Black,
+        style = TextStyle(fontSize = 18.sp),
+        modifier = Modifier.padding(top = 16.dp, bottom = 6.dp)
+    )
+}
 
 //@Composable
 //fun PreviewTemplate(
